e385384d3c162f435c2826ce019e891638040fd2
Erikelpiola <erikelpiola13@gmail.com>
2024-03-21 16:06:55 -0300

    Insercion-Nro de fallos

--- game.py
+++ game.py
@@ -5,7 +5,8 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+max_fails = 4
+fails=0
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
@@ -13,7 +14,7 @@ print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-for i in range(max_attempts):
+while fails<max_fails:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
     #Si llega a pasar que se pulse "Enter", se pedirán letras válidas y no consumirá el intento del jugador
@@ -31,6 +32,7 @@ for i in range(max_attempts):
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+        fails=fails+1
     # Mostrar la palabra parcialmente adivinada
     letters = []
     for letter in secret_word:
@@ -45,5 +47,5 @@ for i in range(max_attempts):
         print(f"¡Felicidades! Has adivinado la palabra secreta:{secret_word}")
         break
 else:
-    print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    print(f"¡Oh no! Has agotado tus {max_fails} intentos.")
     print(f"La palabra secreta era: {secret_word}")
